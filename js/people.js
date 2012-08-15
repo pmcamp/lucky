45855a8b986ec87b026546edf87251312c58ba88
Eric Lo <lxneng@gmail.com>
2012-08-15 17:09:50 +0800

    add more data

--- js/people.js
+++ js/people.js
@@ -1,1 +1,10 @@
-var all_people = ['张三+13111111111', '李四+13888888888'，'PMCamp'，'刘德华+1399999999']
+var all_people = ['张三+13111111111', '李四+13888888888'，'PMCamp'，
+                  '刘德华+1399999999', 'zhengjiahua123456', 'jianyi123456',
+                  'youyou123456', 'liaosimu123456', 'wuxiaomei123456',
+                  'wujianmei123456', 'wushuhe123456', 'wuouming123456',
+                  'wuyihan123456', 'songming123456', 'songhuti123456',
+                  'songbao123456', 'songge123456', 'songjianfei123456',
+                  'xutian123456', 'jiantonghe123456', 'jianshu123456',
+                  'geluhe123456', 'wujianmei', 'wushuhe', 'wuouming',
+                  'wuyiha', 'songm', 'song', 'songba', 'songg', 'songjia',
+                ]
